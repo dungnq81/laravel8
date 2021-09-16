ef1151b0af04f9c652a9fec45404b54abdf84836
dungnq81 <30911891a>
2021-09-16 20:20:48 +0700

    init

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css', {
+        sassOptions: {
+            quietDeps: true,
+        },
+    })
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sourceMaps();
